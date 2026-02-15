a88eec49bd3fbebacd527d85606333a480d91f5e
Kaue <kauesique123@gmail.com>
2026-02-15 20:44:37 -0300

    Configurando ALLOWED_HOSTS para o domínio do Render

--- setup/settings.py
+++ setup/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ALLOWED_HOSTS = [
+    'ats-resume-builder-ipwh.onrender.com', 
+    '127.0.0.1', 
+    'localhost'
+]
